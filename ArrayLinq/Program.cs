3651ad1bc6cdce9b1199f5b90f23bccee6b9494a
Diego Papale <diegopapale@gmail.com>
2021-10-13 05:46:12 -0300

    Teste pratico

---
+++ ArrayLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] vetor = new int[11];
+
+            //leitura dos dados informador pelo usuario
+            Console.WriteLine("Leitura do vetor, informe os números abaixo:");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("Vetor[" + i + "] :");
+                vetor[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int npar = (from n in vetor select n).Where(n => n % 2 == 0).Count();
+            int nimpar = (from n in vetor select n).Where(n => n % 2 > 0).Count();
+
+            Console.WriteLine($"Total de números impares é {nimpar} e total de números pares é {npar}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ arrayDeStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] array = { "abracadabra", "allottee", "assessee", "papagaio", "gato", "cachorro", "papagaio" };
+
+        for (int i = 0; i < array.Length; i++) // Length qtde de palavras ou registros do array
+        {
+            Console.WriteLine(RemoverCaracteresDuplicados(array[i]));
+        }
+    }
+
+    static string RemoverCaracteresDuplicados(string texto) // recebe que está correndo no for
+    {
+        // Armazena as letras, somente um caractere
+        char anterior = ' ' ;
+
+        // armazena o resultado
+        string resultado = "";
+
+        // Percorre cada caractere do texto
+        foreach (char valor in texto)
+        {
+            if (anterior != valor)
+            {
+                anterior = valor;
+                resultado += valor;
+            }
+        }
+
+        return resultado;
+    }
+}
